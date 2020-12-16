de6c9eba404183f193299ef9e867f6a190d9e8fb
Paul Coenen <paul.coenen@g-plus.be>
2020-12-16 18:16:51 +0100

    Desactivation code Alex (tout comme commentaire)

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounterOld.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounterOld.java
@@ -1,11 +1,13 @@
 package com.hemebiotech.analytics;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
+//import java.io.BufferedReader;
+//import java.io.FileReader;
+//import java.io.FileWriter;
 
-public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
+public class AnalyticsCounterOld {
+
+	
+	/*	private static int headacheCount = 0;	// initialize to 0
 	private static int rashCount = 0;		// initialize to 0
 	private static int pupilCount = 0;		// initialize to 0
 	
@@ -39,5 +41,5 @@ public class AnalyticsCounter {
 		writer.write("rash: " + rashCount + "\n");
 		writer.write("dialated pupils: " + pupilCount + "\n");
 		writer.close();
-	}
+	}*/
 }
